99312cf04f0143406fde219ab730f4e1d98fbfb2
EEYakovlev <ilon@mask.com>
2023-11-06 17:13:07 +0300

    add lvls

--- src/components/Step-18/Step18.tsx
+++ src/components/Step-18/Step18.tsx
@@ -23,34 +23,61 @@ function Step18() {
         </header>
 
         <div className={styles.diagram_wrapper}>
-          <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-            <div >
-              high
+
+          <div className={styles.levels_wrapper} >
+            <div style={{ display: 'flex', gap: '10px', alignItems: 'center', transform: 'translateY(-50%)' }}>
+              <div className={styles.lvl_name}>
+                High
+              </div>
+              <div className={styles.diagram_dashed_line} />
             </div>
-            <div className={styles.diagram_dashed_line} />
+
+            <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+
+              <div className={styles.lvl_name}>
+                Medium
+              </div>
+              <div className={styles.diagram_dashed_line} />
+            </div>
+            <div style={{ display: 'flex', gap: '10px', alignItems: 'center', transform: 'translateY(50%)' }}>
+
+              <div className={styles.lvl_name}>
+                Low
+              </div>
+              <div style={{
+                opacity: 0.5,
+                flexBasis: '100%',
+                borderTop: '1px solid #5072F1',
+                height: '1px'
+              }} />
+            </div>
           </div>
 
-          <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+          <div style={{
+            background: 'rgba(217, 88, 76, 0.10)', height: '100%',
+            width: '70%', position: 'absolute', top: '0', right: '0',
+            display: 'flex'
+          }}>
 
-            <div >
-              medium
-            </div>
-            <div className={styles.diagram_dashed_line} />
-          </div>
-          <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-
-            <div >
-              low
-            </div>
             <div style={{
-              flexBasis: '100%',
-              borderTop: '1px solid',
-              height: '1px',
-              transform: 'translateY(50%)'
-            }} />
-          </div>
-        </div>
+              width: '50px',
+              height: '38px',
+              borderRadius: '8px 8px 0px 0px',
+              background: '#29CC7A'
+            }}>
+            </div>
 
+            <div style={{
+              width: '50px',
+              height: '38px',
+              borderRadius: '8px 8px 0px 0px',
+              background: 'red'
+            }}>
+            </div>
+
+          </div>
+
+        </div>
       </div>
     </div>
   );
