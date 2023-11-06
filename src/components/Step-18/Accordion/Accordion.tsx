13aa9403ef48604e59df94d2f3da035b77f3fb5f
EEYakovlev <ilon@mask.com>
2023-11-06 21:50:00 +0300

    add accordion

---
+++ src/components/Step-18/Accordion/Accordion.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { accordionData } from "./AccordionData";
+import { useState } from "react";
+
+export interface AccordionItemProps {
+    title: string,
+    details: string,
+    active: boolean
+}
+
+export const AccordionItem = ({ title, details, active }: AccordionItemProps) => {
+    return (<div style={{
+        display: 'flex', flexDirection: 'column', gap: '12px',
+        borderRadius: '8px',
+        background: '#F4F6FC',
+        padding: '12px 8px'
+    }}>
+        <div style={{
+            fontSize: '14px',
+            fontWeight: '700',
+            lineHeight: '1.3',
+            textAlign: 'left'
+        }}>{title}</div>
+        <div style={{ textAlign: 'left' }}>{details}</div>
+    </div>)
+}
+
+export const Accordion = () => {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const handleClick = (index: number) => {
+        if (selectedIndex === index) {
+            return setSelectedIndex(0);
+        }
+        setSelectedIndex(index);
+    };
+
+    return (<div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        {accordionData.map((data, index) =>
+            <AccordionItem title={data.title} details={data.details} key={index} active={index === selectedIndex} />)}
+    </div>);
+}
--- src/components/Step-18/Step18.tsx
+++ src/components/Step-18/Step18.tsx
@@ -1,14 +1,11 @@
 'use client';
-import { useRouter } from 'next/navigation';
 import styles from './Step.module.scss';
-import { useState } from 'react';
+import { DiagramHigh } from './DiagramHigh';
+import icon_warning from '../../assets/icons/icon_warning.svg'
+import Image from 'next/image';
+import { Accordion } from './Accordion/Accordion';
 
 function Step18() {
-  const router = useRouter();
-  const [isChecked, setIsChecked] = useState(false);
-  const inputChangeHandler = () => {
-    setIsChecked(true);
-  };
 
   return (
     <div>
@@ -19,65 +16,37 @@ function Step18() {
           <h1 className={styles.header_title}>
             Your risk profile
           </h1>
-
         </header>
 
-        <div className={styles.diagram_wrapper}>
-
-          <div className={styles.levels_wrapper} >
-            <div style={{ display: 'flex', gap: '10px', alignItems: 'center', transform: 'translateY(-50%)' }}>
-              <div className={styles.lvl_name}>
-                High
-              </div>
-              <div className={styles.diagram_dashed_line} />
-            </div>
-
-            <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-
-              <div className={styles.lvl_name}>
-                Medium
-              </div>
-              <div className={styles.diagram_dashed_line} />
-            </div>
-            <div style={{ display: 'flex', gap: '10px', alignItems: 'center', transform: 'translateY(50%)' }}>
-
-              <div className={styles.lvl_name}>
-                Low
-              </div>
-              <div style={{
-                opacity: 0.5,
-                flexBasis: '100%',
-                borderTop: '1px solid #5072F1',
-                height: '1px'
-              }} />
-            </div>
-          </div>
-
+        <div style={{
+          display: 'flex',
+          gap: '8px',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <Image src={icon_warning} alt='' />
           <div style={{
-            background: 'rgba(217, 88, 76, 0.10)', height: '100%',
-            width: '70%', position: 'absolute', top: '0', right: '0',
-            display: 'flex'
+            fontSize: ' 14px',
+            fontWeight: '600',
+            textTransform: 'uppercase'
           }}>
-
-            <div style={{
-              width: '50px',
-              height: '38px',
-              borderRadius: '8px 8px 0px 0px',
-              background: '#29CC7A'
-            }}>
-            </div>
-
-            <div style={{
-              width: '50px',
-              height: '38px',
-              borderRadius: '8px 8px 0px 0px',
-              background: 'red'
-            }}>
-            </div>
-
+            High risk data leakage
           </div>
-
         </div>
+
+
+        <DiagramHigh />
+
+        <div style={{
+          fontSize: '16px',
+          fontWeight: '700',
+          lineHeight: '1.2',
+          textAlign: 'left'
+        }}>
+          To reduce your risk, follow the safety guidelines below:
+        </div>
+        <Accordion />
+
       </div>
     </div>
   );
