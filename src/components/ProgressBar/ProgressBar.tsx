8874da3b9b1241ca9dad5998eda254efee4d16a8
MiZhuk <mikhail.avangarde@gmail.com>
2023-11-08 00:04:59 +0300

    check to progress circles

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -2,6 +2,8 @@
 import styles from './ProgressBar.module.scss';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import icon_check from '../../assets/icons/icon_check.svg';
+import Image from 'next/image';
 
 const TOTAL_STEPS = 20;
 
@@ -16,24 +18,69 @@ export const ProgressBar = () => {
     }
   }, [stepNumber]);
 
+  console.log('progress', progress);
+
   return (
     <div style={{ width: '100%', margin: '50px 0', position: 'relative' }}>
-      <button
-        onClick={() => {
-          setProgress((state) => state + 20);
+      {/* <Image
+        src={icon_check}
+        alt=''
+        style={{
+          position: 'absolute',
+          zIndex: '100',
         }}
-      >
-        add
-      </button>
+      /> */}
       <div
         style={{
           transition: 'all 0.5s ease-in-out',
           position: 'absolute',
-          width: `${progress}px`,
+          width: `${progress}%`,
           background: '#5072F1',
           height: '3px',
         }}
       ></div>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          position: 'absolute',
+          top: '50%',
+          transform: ' translateY(-50%)',
+        }}
+      >
+        <div className={[styles.circle, styles.circle_filled].join(' ')}>
+          <Image src={icon_check} alt='' />
+        </div>
+        <div
+          className={
+            progress < 32
+              ? styles.circle
+              : [styles.circle, styles.circle_filled].join(' ')
+          }
+        >
+          {progress > 32 && <Image src={icon_check} alt='' />}
+        </div>
+        <div
+          className={
+            progress < 63
+              ? styles.circle
+              : [styles.circle, styles.circle_filled].join(' ')
+          }
+        >
+          {progress > 63 && <Image src={icon_check} alt='' />}
+        </div>
+        <div
+          className={
+            progress < 94
+              ? styles.circle
+              : [styles.circle, styles.circle_filled].join(' ')
+          }
+        >
+          {progress > 94 && <Image src={icon_check} alt='' />}
+        </div>
+      </div>
       <div
         style={{
           width: '100%',
