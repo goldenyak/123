a3a5dacaa346a32b1c36970a5fe44512998a8242
MiZhuk <mikhail.avangarde@gmail.com>
2023-11-08 23:43:56 +0300

    step18

--- src/components/Step-18/Accordion/AccordionData.ts
+++ src/components/Step-18/Accordion/AccordionData.ts
@@ -1,27 +1,28 @@
 interface AccordionData {
-    title: string,
-    details: string
+  title: string;
+  details: string;
 }
 
 export const accordionData: AccordionData[] = [
-    {
-        title: 'Use VPN Lumos',
-        details: 'VPN encryption hides the contents of Internet traffic from unauthorized reading, allowing you to increase your online security.'
-    },
-    {
-        title: 'Protect your Wi-Fi connection',
-        details: 'VPN encryption hides the contents of Internet traffic from unauthorized reading, allowing you to increase your online security.'
-    },
-    {
-        title: 'Keep passwords securely',
-        details: 'VPN encryption hides the contents of Internet traffic from unauthorized reading, allowing you to increase your online security.'
-    },
-    {
-        title: 'Get regular updates',
-        details: 'VPN encryption hides the contents of Internet traffic from unauthorized reading, allowing you to increase your online security.'
-    },
-    {
-        title: 'Secure Internet Connection',
-        details: 'VPN encryption hides the contents of Internet traffic from unauthorized reading, allowing you to increase your online security.'
-    }
-]
+  {
+    title: 'Use VPN Lumos',
+    details:
+      'VPN encryption hides the contents of Internet traffic from unauthorized reading, allowing you to increase your online security.',
+  },
+  {
+    title: 'Protect your Wi-Fi connection',
+    details: `VPN is an incredibly effective way to shield all your data while connected to a public WiFi. The hacker will be able to see that you are connected, but won't be able to see any details of the traffic transmitted.`,
+  },
+  {
+    title: 'Keep passwords securely',
+    details: `Storing your passwords in browsers is extremely insecure. It is better to use special programs to protect them.`,
+  },
+  {
+    title: 'Get regular updates',
+    details: `Keeping your software and apps up-to-date is essential for maintaining the security.`,
+  },
+  {
+    title: 'Secure Internet Connection',
+    details: `You should always check all the links you follow and whether the connection is secure on a particular site.`,
+  },
+];
