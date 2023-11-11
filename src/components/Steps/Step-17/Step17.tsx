7de035ce98e2bb6a46e680ecc626ab3c1a0747b0
MiZhuk <mikhail.avangarde@gmail.com>
2023-11-11 22:57:56 +0300

    step17-superman-slide-animations

--- src/components/Steps/Step-17/Step17.tsx
+++ src/components/Steps/Step-17/Step17.tsx
@@ -1,170 +1,78 @@
 'use client';
-import { useRouter } from 'next/navigation';
 import styles from './Step.module.scss';
 import Image from 'next/image';
 import ProgressBar from '../../ProgressBar/ProgressBar';
 import review_stars from '../../../assets/images/review_stars.png';
+import superman_image from '../../../assets/images/superman.png';
 
 function Step17() {
   return (
     <div style={{ position: 'relative', boxSizing: 'border-box' }}>
-      <div className={styles.main_wrapper}>
-        <h1 className={styles.header}>
-          VPN Lumos is an incredibly effective way to shield all your data
-        </h1>
-        <div className={styles.review_wrapper}>
-          <div className={styles.review_content}>
-            <h3 className={styles.review_title}>Love this app</h3>
-            <Image
-              src={review_stars}
-              alt='review_stars'
-              className={styles.image}
-              width={90}
-              height={15}
-            />
-            <p className={styles.review_text}>
-              Easy to use, reliable & affordable
-            </p>
-            <div className={styles.review_description}>
-              <span>figrx</span>
-              <span>Oct 13, 2023</span>
-            </div>
-          </div>
-        </div>
+      <h1 className={styles.header}>
+        VPN Lumos is an incredibly effective way to shield all your data
+      </h1>
 
-        <div className={styles.review_wrapper}>
-          <div className={styles.review_content}>
-            <h3 className={styles.review_title}>Stunning</h3>
-            <Image
-              src={review_stars}
-              alt='review_stars'
-              className={styles.image}
-              width={90}
-              height={15}
-            />
-            <p className={styles.review_text}>
-              Great service at a great price. Very happy with this very neat
-              tool that helps keep me secured and protected. Love it!
-            </p>
-            <div className={styles.review_description}>
-              <span>Dinar-ka</span>
-              <span>Oct 13, 2023</span>
-            </div>
-          </div>
-        </div>
+      <Image
+        src={superman_image}
+        alt=''
+        style={{
+          width: '100%',
+          height: 'auto',
+          padding: '0 35px',
+        }}
+      />
 
-        <div className={styles.review_wrapper}>
-          <div className={styles.review_content}>
-            <h3 className={styles.review_title}>Stunning</h3>
-            <Image
-              src={review_stars}
-              alt='review_stars'
-              className={styles.image}
-              width={90}
-              height={15}
-            />
-            <p className={styles.review_text}>
-              Great service at a great price. Very happy with this very neat
-              tool that helps keep me secured and protected. Love it!
-            </p>
-            <div className={styles.review_description}>
-              <span>Dinar-ka</span>
-              <span>Oct 13, 2023</span>
-            </div>
+      <div
+        className={[styles.review_wrapper, styles.delayed_slide_left].join(' ')}
+      >
+        <div className={styles.review_content}>
+          <h3 className={styles.review_title}>Love this app</h3>
+          <Image
+            src={review_stars}
+            alt='review_stars'
+            className={styles.image}
+            width={90}
+            height={15}
+          />
+          <p className={styles.review_text}>
+            Easy to use, reliable & affordable
+          </p>
+          <div className={styles.review_description}>
+            <span>figrx</span>
+            <span>Oct 13, 2023</span>
           </div>
         </div>
-
-        <div className={styles.review_wrapper}>
-          <div className={styles.review_content}>
-            <h3 className={styles.review_title}>Stunning</h3>
-            <Image
-              src={review_stars}
-              alt='review_stars'
-              className={styles.image}
-              width={90}
-              height={15}
-            />
-            <p className={styles.review_text}>
-              Great service at a great price. Very happy with this very neat
-              tool that helps keep me secured and protected. Love it!
-            </p>
-            <div className={styles.review_description}>
-              <span>Dinar-ka</span>
-              <span>Oct 13, 2023</span>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.review_wrapper}>
-          <div className={styles.review_content}>
-            <h3 className={styles.review_title}>Stunning</h3>
-            <Image
-              src={review_stars}
-              alt='review_stars'
-              className={styles.image}
-              width={90}
-              height={15}
-            />
-            <p className={styles.review_text}>
-              Great service at a great price. Very happy with this very neat
-              tool that helps keep me secured and protected. Love it!
-            </p>
-            <div className={styles.review_description}>
-              <span>Dinar-ka</span>
-              <span>Oct 13, 2023</span>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.review_wrapper}>
-          <div className={styles.review_content}>
-            <h3 className={styles.review_title}>Stunning</h3>
-            <Image
-              src={review_stars}
-              alt='review_stars'
-              className={styles.image}
-              width={90}
-              height={15}
-            />
-            <p className={styles.review_text}>
-              Great service at a great price. Very happy with this very neat
-              tool that helps keep me secured and protected. Love it!
-            </p>
-            <div className={styles.review_description}>
-              <span>Dinar-ka</span>
-              <span>Oct 13, 2023</span>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={styles.review_wrapper}
-          style={{
-            marginBottom: '150px',
-          }}
-        >
-          <div className={styles.review_content}>
-            <h3 className={styles.review_title}>Stunning</h3>
-            <Image
-              src={review_stars}
-              alt='review_stars'
-              className={styles.image}
-              width={90}
-              height={15}
-            />
-            <p className={styles.review_text}>
-              Great service at a great price. Very happy with this very neat
-              tool that helps keep me secured and protected. Love it!
-            </p>
-            <div className={styles.review_description}>
-              <span>Dinar-ka</span>
-              <span>Oct 13, 2023</span>
-            </div>
-          </div>
-        </div>
-        <p className={styles.checking_answers}>Checking your answers...</p>
-        <ProgressBar path='/quiz?q=18' />
       </div>
+
+      <div
+        className={[styles.review_wrapper, styles.slide_in].join(' ')}
+        style={{
+          right: '-600px',
+          opacity: '0',
+        }}
+      >
+        <div className={styles.review_content}>
+          <h3 className={styles.review_title}>Stunning</h3>
+          <Image
+            src={review_stars}
+            alt='review_stars'
+            className={styles.image}
+            width={90}
+            height={15}
+          />
+          <p className={styles.review_text}>
+            Great service at a great price. Very happy with this very neat tool
+            that helps keep me secured and protected. Love it!
+          </p>
+          <div className={styles.review_description}>
+            <span>Dinar-ka</span>
+            <span>Oct 13, 2023</span>
+          </div>
+        </div>
+      </div>
+
+      <p className={styles.checking_answers}>Checking your answers...</p>
+      <ProgressBar path='/quiz?q=18' />
     </div>
   );
 }
