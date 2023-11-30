f220ad7428226cb3a746e21368e8c81147fbc629
MiZhuk <mikhail.avangarde@gmail.com>
2023-11-30 18:03:19 +0300

    persist store

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { config } from '@/utils/utils';
 import { IStepConfig } from '@/components/StepContent/types';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
@@ -54,93 +55,101 @@ initSteps[config.mainScreen.noRedirectTo] = {
   locked: false,
 };
 
-export const useStore = create<State & Action>((set, get) => ({
-  currentStepId: config.steps[0].id,
-  answers: initAnswers,
-  steps: initSteps,
-  setCurrentStepId: (currentStepId) => {
-    set({ currentStepId });
-  },
-  goTo: (router, stepId) => {
-    const nextStep = get().steps[stepId];
-    set({
-      steps: {
-        ...get().steps,
-        [nextStep.id]: { ...get().steps[nextStep.id], locked: false },
+export const useStore = create<State & Action>()(
+  persist(
+    (set, get) => ({
+      currentStepId: config.steps[0].id,
+      answers: initAnswers,
+      steps: initSteps,
+      setCurrentStepId: (currentStepId) => {
+        set({ currentStepId });
       },
-    });
-    router.push(`?q=${stepId}`);
-  },
-  next: (router) => {
-    const currentStep = get().steps[get().currentStepId];
-    if (currentStep?.nextButton?.redirectTo) {
-      const nextStep = get().steps[currentStep.nextButton.redirectTo];
-      set({
-        steps: {
-          ...get().steps,
-          [nextStep.id]: { ...get().steps[nextStep.id], locked: false },
-        },
-        currentStepId: nextStep.id,
-      });
-      router.push(`?q=${nextStep.id}`);
-    }
-  },
-  back: (router) => {
-    const currentStep = get().steps[get().currentStepId];
-    const prevStepId = currentStep.prevStep;
-    set({ currentStepId: prevStepId || config.steps[0].id });
-    set({
-      answers: { ...get().answers, [get().currentStepId]: [] },
-    });
-    prevStepId ? router.push(`?q=${prevStepId}`) : router.push(`/`);
-    console.log(get().answers);
-  },
-  isLastStep: () => !!get().steps[get().currentStepId].lastStep,
-  isLocked: (stepId) => get().steps[stepId]?.locked,
-  isNextDisabled: () => !!get().steps[get().currentStepId]?.disableNext,
-  addAnswer: (value) => {
-    if (get().answers[get().currentStepId].includes(value)) {
-      set({
-        answers: {
-          ...get().answers,
-          [get().currentStepId]: get().answers[get().currentStepId].filter(
-            (item) => item !== value,
-          ),
-        },
-      });
-    } else {
-      set({
-        answers: {
-          ...get().answers,
-          [get().currentStepId]: [...get().answers[get().currentStepId], value],
-        },
-      });
-    }
+      goTo: (router, stepId) => {
+        const nextStep = get().steps[stepId];
+        set({
+          steps: {
+            ...get().steps,
+            [nextStep.id]: { ...get().steps[nextStep.id], locked: false },
+          },
+        });
+        router.push(`?q=${stepId}`);
+      },
+      next: (router) => {
+        const currentStep = get().steps[get().currentStepId];
+        if (currentStep?.nextButton?.redirectTo) {
+          const nextStep = get().steps[currentStep.nextButton.redirectTo];
+          set({
+            steps: {
+              ...get().steps,
+              [nextStep.id]: { ...get().steps[nextStep.id], locked: false },
+            },
+            currentStepId: nextStep.id,
+          });
+          router.push(`?q=${nextStep.id}`);
+        }
+      },
+      back: (router) => {
+        const currentStep = get().steps[get().currentStepId];
+        const prevStepId = currentStep.prevStep;
+        set({ currentStepId: prevStepId || config.steps[0].id });
+        set({
+          answers: { ...get().answers, [get().currentStepId]: [] },
+        });
+        prevStepId ? router.push(`?q=${prevStepId}`) : router.push(`/`);
+      },
+      isLastStep: () => !!get().steps[get().currentStepId].lastStep,
+      isLocked: (stepId) => get().steps[stepId]?.locked,
+      isNextDisabled: () => !!get().steps[get().currentStepId]?.disableNext,
+      addAnswer: (value) => {
+        if (get().answers[get().currentStepId].includes(value)) {
+          set({
+            answers: {
+              ...get().answers,
+              [get().currentStepId]: get().answers[get().currentStepId].filter(
+                (item) => item !== value,
+              ),
+            },
+          });
+        } else {
+          set({
+            answers: {
+              ...get().answers,
+              [get().currentStepId]: [
+                ...get().answers[get().currentStepId],
+                value,
+              ],
+            },
+          });
+        }
 
-    if (get().steps[get().currentStepId].hasOwnProperty('disableNext')) {
-      if (get().answers[get().currentStepId].length === 0) {
-        set({
-          steps: {
-            ...get().steps,
-            [get().currentStepId]: {
-              ...get().steps[get().currentStepId],
-              disableNext: true,
-            },
-          },
-        });
-      }
-      if (get().answers[get().currentStepId].length > 0) {
-        set({
-          steps: {
-            ...get().steps,
-            [get().currentStepId]: {
-              ...get().steps[get().currentStepId],
-              disableNext: false,
-            },
-          },
-        });
-      }
-    }
-    console.log(get().answers);
-  },
-}));
+        if (get().steps[get().currentStepId].hasOwnProperty('disableNext')) {
+          if (get().answers[get().currentStepId].length === 0) {
+            set({
+              steps: {
+                ...get().steps,
+                [get().currentStepId]: {
+                  ...get().steps[get().currentStepId],
+                  disableNext: true,
+                },
+              },
+            });
+          }
+          if (get().answers[get().currentStepId].length > 0) {
+            set({
+              steps: {
+                ...get().steps,
+                [get().currentStepId]: {
+                  ...get().steps[get().currentStepId],
+                  disableNext: false,
+                },
+              },
+            });
+          }
+        }
+      },
+    }),
+    {
+      name: 'quiz-storage',
+    },
+  ),
+);
